59247ffe30c2ac54c28eb2710d301764d3a768e0
navya2006 <navya2006.github.com>
2020-11-01 16:58:56 +0530

    trying to make navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+// disappearing navbar
+
+jQuery(document).on("scroll", function () {
+    if ($(document).scrollTop() > 200) {
+        $(".navb").css("background-color","rgba(0,0,0,0.6)");
+        $(".disappear").css("top","0");
+    } else {
+        $(".navb").css("background","none");
+        $(".disappear").css("top","-200");
+    }
+}); 
